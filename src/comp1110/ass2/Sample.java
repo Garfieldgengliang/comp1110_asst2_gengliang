7cedabc2af00c3ff2b19dc29cf26227087ab990c
Joel Zekkyle <u6708832@anu.edu.au>
2019-03-28 18:15:59 +1100

    28 March Thursday Meeting

---
+++ src/comp1110/ass2/Sample.java
@@ -0,0 +1,49 @@
+package comp1110.ass2;
+
+import javafx.geometry.Orientation;
+import javafx.scene.chart.PieChart;
+
+public class Sample {
+    Piece piece;
+    Spot spot;
+    char Orientation;
+    char left;
+    char right;
+    char up;
+    char down;
+    char centre;
+
+
+    Sample (String placementString){
+        this.piece = piece;
+        this.spot = spot;
+        this.Orientation = Orientation;Tile
+        char [] output = new char[5];
+        output
+    }
+
+    public void tileProperties(char Orientation){
+        this.Orientation = Orientation;
+        this.left = piece.
+    }
+
+    public char getUp() {
+        return up;
+    }
+
+    public String toString(){
+        return (piece+spot+Orientation);
+    }
+}
+
+public class Spot{
+    int x;
+    int y;
+    char property;
+    char direction;
+
+
+
+
+
+}
