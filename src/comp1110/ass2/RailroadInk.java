43b12adddc01ecd32581f5089e3b01df39b0ab6a
Gengliang Li <u6799959@anu.edu.au>
2019-04-25 11:18:50 +1000

    modify task6

--- src/comp1110/ass2/RailroadInk.java
+++ src/comp1110/ass2/RailroadInk.java
@@ -160,7 +160,7 @@ public class RailroadInk {
 
     }
 
-    //testcommit
+    //testCommit
      public static  boolean isvalidExit(String tilePlacementString){
         // this method is to check whether a tileplacement string is legally connected to an exit
          // this means first we check if a tile is neighboring an exit, then we check if the connection is legal
