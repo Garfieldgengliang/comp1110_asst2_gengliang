c6631f698ff0640d8b0c06014b89bbdeedf260c4
Gengliang Li <u6799959@anu.edu.au>
2019-05-05 21:44:14 +1000

    commit

--- src/comp1110/ass2/selfTest.java
+++
@@ -1,9 +0,0 @@
-package comp1110.ass2;
-
-public class selfTest
-{
-    public static void main(String[] args) {
-        String thisTest = "A0B00";
-        System.out.println(RailroadInk.isValidPlacementSequence(thisTest));
-    }
-}
