0e0d9444a88fada70f1a1be104b34720fc1dce77
Gengliang Li <u6799959@anu.edu.au>
2019-05-11 21:02:13 +1000

    commit

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -8,11 +8,9 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -309,9 +307,7 @@ public class Viewer extends Application {
             homeY = getLayoutY();
 
             setOnMouseClicked(event -> {
-                if(event.getButton() == MouseButton.SECONDARY){
-                    setTileRotate();
-                }
+
             });
 
             setOnMousePressed(event -> {
@@ -338,26 +334,7 @@ public class Viewer extends Application {
             });
         }
 
-        public void setTileRotate(){
-            int currentOri = orientation%8;
-            if(currentOri == 0||currentOri == 1||currentOri == 2){
-                setRotate(90*(currentOri+1));
-            }
-            else if(currentOri == 3){
-                setRotate(0);
-                setScaleX(-1);
-            }
-            else if(currentOri == 4||currentOri == 5||currentOri==6){
-                setRotate(0);
-                setScaleX(-1);
-                setRotate(90*(currentOri-3));
-            }
-            else if(currentOri==7){
-                setRotate(0);
-                setScaleX(1);
-            }
-            orientation++;
-        }
+       
 
         private void snapToGrid(){
             if(onBoard()){
